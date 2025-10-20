23336a44eb1100f344fbe9ab3e02525d4ab8cc7d
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-10-20 07:59:24 -0600

    chore: add instructor tabs slot

--- src/pageWrapper/PageWrapper.tsx
+++ src/pageWrapper/PageWrapper.tsx
@@ -1,13 +1,13 @@
 import { useIntl } from '@openedx/frontend-base';
 import messages from './messages';
-import InstructorTabs from '../instructorTabs/InstructorTabs';
+import InstructorTabsSlot from '../slots/instructorTabsSlot/instructorTabsSlot';
 
 const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   const { formatMessage } = useIntl();
   return (
     <div className="container-xl">
       <h2>{formatMessage(messages.pageTitle)}</h2>
-      <InstructorTabs />
+      <InstructorTabsSlot />
       {children}
     </div>
   );
