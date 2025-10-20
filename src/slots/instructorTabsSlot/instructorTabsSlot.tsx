4226d56948e746c3d13c7dbcff0bb0d94c843964
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-10-20 07:59:24 -0600

    chore: add instructor tabs slot

---
+++ src/slots/instructorTabsSlot/instructorTabsSlot.tsx
@@ -0,0 +1,12 @@
+import { Slot } from '@openedx/frontend-base';
+import { InstructorTabs } from '../../instructorTabs';
+
+export const InstructorTabsSlot = () => (
+  <Slot
+    id="org.openedx.frontend.slot.instructor.tabs.v1"
+  >
+    <InstructorTabs />
+  </Slot>
+);
+
+export default InstructorTabsSlot;
