d4e8f2f373cbc2e60d61cbb6c1451b0905197704
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-11-11 16:00:29 -0600

    fix: useParams instead on complicated logic

--- src/instructorTabs/app.tsx
+++ src/instructorTabs/app.tsx
@@ -3,30 +3,30 @@ import TabSlot from '../slots/instructorTabsSlot/TabSlot';
 import { TabProps } from './InstructorTabs';
 
 enum InstructorTabKeys {
-  COURSE_INFO = 'courseInfo',
+  COURSE_INFO = 'course_info',
   ENROLLMENTS = 'enrollments',
-  COURSE_TEAM = 'courseTeam',
+  COURSE_TEAM = 'course_team',
   GRADING = 'grading',
-  DATE_EXTENSIONS = 'dateExtensions',
-  DATA_DOWNLOADS = 'dataDownloads',
-  OPEN_RESPONSES = 'openResponses',
+  DATE_EXTENSIONS = 'date_extensions',
+  DATA_DOWNLOADS = 'data_downloads',
+  OPEN_RESPONSES = 'open_responses',
   CERTIFICATES = 'certificates',
   COHORTS = 'cohorts',
-  SPECIAL_EXAMS = 'specialExams',
+  SPECIAL_EXAMS = 'special_exams',
 }
 
 // example of tabs response from an API, should be refactored to react query when backend is ready
 const tabData: TabProps[] = [
-  { tab_id: InstructorTabKeys.COURSE_INFO, url: 'course_info', title: 'Course Info' },
-  { tab_id: InstructorTabKeys.ENROLLMENTS, url: 'enrollments', title: 'Enrollments' },
-  { tab_id: InstructorTabKeys.COURSE_TEAM, url: 'course_team', title: 'Course Team' },
-  { tab_id: InstructorTabKeys.GRADING, url: 'grading', title: 'Grading' },
-  { tab_id: InstructorTabKeys.DATE_EXTENSIONS, url: 'date_extensions', title: 'Date Extensions' },
-  { tab_id: InstructorTabKeys.DATA_DOWNLOADS, url: 'data_downloads', title: 'Data Downloads' },
-  { tab_id: InstructorTabKeys.OPEN_RESPONSES, url: 'open_responses', title: 'Open Responses' },
-  { tab_id: InstructorTabKeys.CERTIFICATES, url: 'certificates', title: 'Certificates' },
-  { tab_id: InstructorTabKeys.COHORTS, url: 'cohorts', title: 'Cohorts' },
-  { tab_id: InstructorTabKeys.SPECIAL_EXAMS, url: 'special_exams', title: 'Special Exams' },
+  { tab_id: InstructorTabKeys.COURSE_INFO, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/course_info', title: 'Course Info' },
+  { tab_id: InstructorTabKeys.ENROLLMENTS, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/enrollments', title: 'Enrollments' },
+  { tab_id: InstructorTabKeys.COURSE_TEAM, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/course_team', title: 'Course Team' },
+  { tab_id: InstructorTabKeys.GRADING, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/grading', title: 'Grading' },
+  { tab_id: InstructorTabKeys.DATE_EXTENSIONS, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/date_extensions', title: 'Date Extensions' },
+  { tab_id: InstructorTabKeys.DATA_DOWNLOADS, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/data_downloads', title: 'Data Downloads' },
+  { tab_id: InstructorTabKeys.OPEN_RESPONSES, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/open_responses', title: 'Open Responses' },
+  { tab_id: InstructorTabKeys.CERTIFICATES, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/certificates', title: 'Certificates' },
+  { tab_id: InstructorTabKeys.COHORTS, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/cohorts', title: 'Cohorts' },
+  { tab_id: InstructorTabKeys.SPECIAL_EXAMS, url: 'http://apps.local.openedx.io:8081/instructor/course-v1:OpenedX+DemoX+DemoCourse/special_exams', title: 'Special Exams' },
 ];
 
 export const tabSlots: SlotOperation[] = tabData.map(({ tab_id, title, url }: TabProps) => ({
