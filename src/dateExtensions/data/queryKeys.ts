8726fc3279a418e526b00b60d3c31924d9131c18
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-11-14 11:11:23 -0600

    chore: improving querykeys

--- src/dateExtensions/data/queryKeys.ts
+++ src/dateExtensions/data/queryKeys.ts
@@ -1,6 +1,8 @@
 import { appId } from '../../constants';
+import { PaginationQueryKeys } from './api';
 
 export const dateExtensionsQueryKeys = {
   all: [appId, 'dateExtensions'] as const,
-  byCourse: (courseId: string) => [appId, 'dateExtensions', courseId] as const,
+  byCourse: (courseId: string) => [...dateExtensionsQueryKeys.all, courseId] as const,
+  byCoursePaginated: (courseId: string, pagination: PaginationQueryKeys) => [...dateExtensionsQueryKeys.byCourse(courseId), pagination.page] as const,
 };
