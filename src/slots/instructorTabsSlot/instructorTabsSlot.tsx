9587d78f6b0ff2686f9fa712debea7e3fbee50af
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-11-20 11:23:47 -0600

    fix: update read me and rename instructor tab

--- src/slots/instructorTabsSlot/instructorTabsSlot.tsx
+++ src/slots/instructorTabsSlot/instructorTabsSlot.tsx
@@ -1,5 +1,14 @@
 import { Slot } from '@openedx/frontend-base';
-import InstructorTabs from '../../instructorTabs/InstructorTabs';
+import InstructorTabs, { TabProps } from '../../instructorTabs/InstructorTabs';
+
+// This component will be a placeholder/dummy component just to retrieve Tab props
+// Since we are using a slot-based architecture and Paragon is passing Tabs/Tab through
+// We can't have context provider between Tabs and Tab when rendering it should be direct parent/children relation
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export const InstructorTab = (_props: TabProps) => {
+  return null;
+};
 
 export const InstructorTabsSlot = () => (
   <Slot id="org.openedx.frontend.slot.instructor.tabs.v1">
