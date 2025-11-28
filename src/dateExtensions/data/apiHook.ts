59540b0eec682f9157a38ea2bbbd065834041b8a
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-11-28 14:04:53 -0500

    feat: search field

--- src/dateExtensions/data/apiHook.ts
+++ src/dateExtensions/data/apiHook.ts
@@ -1,11 +1,12 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
-import { getDateExtensions, PaginationQueryKeys, resetDateExtension, addDateExtension, getGradedSubsections } from './api';
+import { getDateExtensions, resetDateExtension, addDateExtension, getGradedSubsections, DateExtensionQueryParams } from './api';
 import { dateExtensionsQueryKeys, gradedSubsectionsQueryKeys } from './queryKeys';
 
-export const useDateExtensions = (courseId: string, pagination: PaginationQueryKeys) => (
+export const useDateExtensions = (courseId: string, params: DateExtensionQueryParams) => (
   useQuery({
-    queryKey: dateExtensionsQueryKeys.byCoursePaginated(courseId, pagination),
-    queryFn: () => getDateExtensions(courseId, pagination),
+    queryKey: dateExtensionsQueryKeys.byCoursePaginated(courseId, params),
+    queryFn: () => getDateExtensions(courseId, params),
+    enabled: !!courseId, // Only run when courseId is available
   })
 );
 
