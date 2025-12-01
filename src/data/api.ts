27f5509aa4df6e8d09e2829a0079ae5d87bc7d7e
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-12-01 13:26:58 -0600

    feat: detailed assessments table

--- src/data/api.ts
+++ src/data/api.ts
@@ -8,3 +8,12 @@ export const getOpenResponsesData = async (courseId: string) => {
   const { data } = await getAuthenticatedHttpClient(appId).get(url);
   return camelCaseObject(data);
 };
+
+export const getDetailAssessmentsData = async (
+  courseId: string,
+  params: Record<string, string | number | boolean> = {},
+) => {
+  const url = `${getApiBaseUrl()}/api/instructor/v2/courses/${courseId}/open-responses/assessments`;
+  const { data } = await getAuthenticatedHttpClient(appId).get(url, { params });
+  return camelCaseObject(data);
+};
--- src/data/apiHook.ts
+++ src/data/apiHook.ts
@@ -1,11 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
-import { getOpenResponsesData } from './api';
+import { getDetailAssessmentsData, getOpenResponsesData } from './api';
 import { openResponsesQueryKeys } from './queryKeys';
 
 export const useOpenResponsesData = (courseId: string) => (
   useQuery({
     queryKey: openResponsesQueryKeys.byCourse(courseId),
     queryFn: () => getOpenResponsesData(courseId),
+    enabled: !!courseId,
+  })
+);
+
+export const useDetailAssessmentsData = (courseId: string, params: Record<string, string | number | boolean> = {}) => (
+  useQuery({
+    queryKey: openResponsesQueryKeys.list(courseId, params),
+    queryFn: () => getDetailAssessmentsData(courseId, params),
     enabled: !!courseId,
   })
 );
