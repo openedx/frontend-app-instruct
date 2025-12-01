cfc958494b81e3d7a412d44a642410195cb04072
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-12-01 13:26:58 -0600

    feat: detailed assessments table

--- src/openResponses/data/queryKeys.ts
+++ src/openResponses/data/queryKeys.ts
@@ -3,5 +3,6 @@ import { appId } from '@src/constants';
 export const openResponsesQueryKeys = {
   all: [appId, 'openResponses'],
   byCourse: (courseId: string) => [...openResponsesQueryKeys.all, courseId],
+  list: (courseId: string, params: Record<string, string | number | boolean> = {}) => [...openResponsesQueryKeys.byCourse(courseId), 'list', ...Object.entries(params).flat()],
   summary: (courseId: string) => [...openResponsesQueryKeys.byCourse(courseId), 'summary'],
 };
