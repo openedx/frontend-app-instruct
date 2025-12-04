f1205c4e0b8559469bbea7c2c8bb0ed5dcf58fc3
jacobo-dominguez-wgu <jacobo.dominguez@wgu.edu>
2025-12-04 16:45:54 -0600

    feat: creating toast manager

--- src/Main.tsx
+++ src/Main.tsx
@@ -6,16 +6,19 @@ import './main.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Outlet } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastManagerProvider } from './providers/ToastManagerProvider';
 
 const queryClient = new QueryClient();
 
 const Main = () => (
   <CurrentAppProvider appId={appId}>
     <QueryClientProvider client={queryClient}>
-      <main>
-        <Outlet />
-        { getAppConfig(appId).NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} /> }
-      </main>
+      <ToastManagerProvider>
+        <main>
+          <Outlet />
+          { getAppConfig(appId).NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} /> }
+        </main>
+      </ToastManagerProvider>
     </QueryClientProvider>
   </CurrentAppProvider>
 );
