0c3a19f6e72bec8d498a9e4953d2765310824c7e
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2025-12-22 13:09:47 -0600

    fix: unit test

--- src/setupTest.js
+++ src/setupTest.js
@@ -3,3 +3,19 @@ import { mergeSiteConfig } from '@openedx/frontend-base';
 import '@testing-library/jest-dom';
 
 mergeSiteConfig(siteConfig);
+
+class ResizeObserver {
+  observe() {
+    // do nothing
+  }
+
+  unobserve() {
+    // do nothing
+  }
+
+  disconnect() {
+    // do nothing
+  }
+}
+
+window.ResizeObserver = ResizeObserver;
