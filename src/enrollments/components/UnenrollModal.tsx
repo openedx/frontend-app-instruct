1f1a95e9c1896f34069bf651624d64570146a903
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2026-01-13 16:56:54 -0600

    feat: add/unenroll learners

--- src/enrollments/components/UnenrollModal.tsx
+++ src/enrollments/components/UnenrollModal.tsx
@@ -1,4 +1,8 @@
+import { useIntl } from '@openedx/frontend-base';
+import { Button, ModalDialog } from '@openedx/paragon';
+import messages from '../messages';
 import { Learner } from '../types';
+
 interface UnenrollModalProps {
   learner: Learner | null,
   isOpen: boolean,
@@ -6,14 +10,27 @@ interface UnenrollModalProps {
 }
 
 const UnenrollModal = ({ learner, isOpen, onClose }: UnenrollModalProps) => {
-  console.log(learner, isOpen);
+  const intl = useIntl();
 
   if (!isOpen || learner === null) {
     onClose();
     return null;
   }
 
-  return <div>Unenroll Modal</div>;
+  return (
+    <ModalDialog isOpen={isOpen} onClose={onClose} title={intl.formatMessage(messages.unenrollLearners)} isOverflowVisible={false}>
+      <ModalDialog.Header>
+        <h3 className="text-primary-500">{intl.formatMessage(messages.unenrollLearnerTitle)}</h3>
+      </ModalDialog.Header>
+      <ModalDialog.Body className="py-4">
+        <p>{intl.formatMessage(messages.unenrollLearnersConfirmation, { name: learner.fullName })}</p>
+      </ModalDialog.Body>
+      <ModalDialog.Footer>
+        <Button variant="tertiary" onClick={onClose}>{intl.formatMessage(messages.cancelButton)}</Button>
+        <Button className="ml-2">{intl.formatMessage(messages.unenrollButton)}</Button>
+      </ModalDialog.Footer>
+    </ModalDialog>
+  );
 };
 
 export default UnenrollModal;
