73d9f18c6019aa2f6e1656400e2ec69f9dfb7c00
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2026-01-29 13:59:49 -0600

    refactor: small change in order label values

--- src/openResponses/components/OpenResponsesSummary.tsx
+++ src/openResponses/components/OpenResponsesSummary.tsx
@@ -1,9 +1,19 @@
 import { useParams } from 'react-router-dom';
 import { useIntl } from '@openedx/frontend-base';
-import { Icon } from '@openedx/paragon';
+import { Icon, Stack } from '@openedx/paragon';
 import { EditNote, ViewDay } from '@openedx/paragon/icons';
 import { useOpenResponsesData } from '@src/openResponses/data/apiHook';
 import messages from '@src/openResponses/messages';
+
+const summaryOrder = [
+  'totalResponses',
+  'training',
+  'peer',
+  'self',
+  'waiting',
+  'staff',
+  'finalGradeReceived',
+];
 
 const OpenResponsesSummary = () => {
   const intl = useIntl();
@@ -12,85 +22,46 @@ const OpenResponsesSummary = () => {
   const {
     totalUnits = 0,
     totalAssessments = 0,
-    totalResponses = 0,
-    training = 0,
-    peer = 0,
-    self = 0,
-    waiting = 0,
-    staff = 0,
-    finalGradeReceived = 0,
   } = data;
 
   return (
     <>
       <h3 className="text-primary-700">{intl.formatMessage(messages.summaryTitle)}</h3>
-      <div className="container-mw-xl">
-        <div className="row x-small">
-          <div className="col-sm-3 col-md-2 col-xl-1">
-            <p className="mb-2">{intl.formatMessage(messages.totalUnits)}</p>
+      <Stack direction="horizontal" gap={3}>
+        <div>
+          <div className="x-small text-gray-500">
+            <p className="mb-0">{intl.formatMessage(messages.totalUnits)}</p>
           </div>
-          <div className="col-sm-4 col-md-3 col-xl-2">
-            <p className="mb-2">{intl.formatMessage(messages.totalAssessments)}</p>
-          </div>
-        </div>
-        <div className="row lead">
-          <div className="col-sm-3 col-md-2 col-xl-1 d-flex align-items-center">
+          <div className="d-flex align-items-center lead">
             <Icon src={ViewDay} />
             <p className="ml-2 mb-0 text-primary-500">{totalUnits}</p>
           </div>
-          <div className="col-sm-4 col-md-3 col-xl-2 d-flex align-items-center">
+        </div>
+        <div>
+          <div className="x-small text-gray-500">
+            <p className="mb-0">{intl.formatMessage(messages.totalAssessments)}</p>
+          </div>
+          <div className="d-flex align-items-center lead">
             <Icon src={EditNote} size="lg" />
             <p className="ml-2 mb-0 text-primary-500">{totalAssessments}</p>
           </div>
         </div>
-      </div>
-      <div className="container-mw-xl mt-3">
-        <div className="row align-items-end x-small">
-          <div className="col-sm-2 col-lg-1">
-            <p className="mb-2">{intl.formatMessage(messages.totalResponses)}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p className="mb-2">{intl.formatMessage(messages.training)}</p>
-          </div>
-          <div className="col-sm-1">
-            <p className="mb-2">{intl.formatMessage(messages.peer)}</p>
-          </div>
-          <div className="col-sm-1">
-            <p className="mb-2">{intl.formatMessage(messages.self)}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p className="mb-2">{intl.formatMessage(messages.waiting)}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p className="mb-2">{intl.formatMessage(messages.staff)}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p className="mb-2">{intl.formatMessage(messages.finalGradeReceived)}</p>
-          </div>
-        </div>
-        <div className="row lead text-primary-500">
-          <div className="col-sm-2 col-lg-1">
-            <p>{totalResponses}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p>{training}</p>
-          </div>
-          <div className="col-sm-1">
-            <p>{peer}</p>
-          </div>
-          <div className="col-sm-1">
-            <p>{self}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p>{waiting}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p>{staff}</p>
-          </div>
-          <div className="col-sm-2 col-lg-1">
-            <p>{finalGradeReceived}</p>
-          </div>
-        </div>
+      </Stack>
+      <div className="mt-3 row w-md-75 w-lg-50 align-items-end">
+        {
+          summaryOrder.map((key) => (
+            <div className="col" key={key}>
+              <div className="x-small text-gray-500">
+                <p className="mb-2">{intl.formatMessage(messages[key as keyof typeof messages])}</p>
+              </div>
+              <div className="lead text-primary-500">
+                <p>
+                  {data[key as keyof typeof data] ?? 0}
+                </p>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   );
