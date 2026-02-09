666f991b22e1aea4cdfa92d5d7dfd8cdb3e2db1a
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2026-02-09 12:02:24 -0600

    style: fixing margins

--- src/courseInfo/CourseInfoPage.tsx
+++ src/courseInfo/CourseInfoPage.tsx
@@ -1,11 +1,10 @@
-import { Container } from '@openedx/paragon';
 import { GeneralCourseInfo } from '@src/courseInfo/components/generalCourseInfo';
 
 const CourseInfoPage = () => {
   return (
-    <Container className="mt-4.5 mb-4" fluid>
+    <>
       <GeneralCourseInfo />
-    </Container>
+    </>
   );
 };
 
