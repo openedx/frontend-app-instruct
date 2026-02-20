3e442d34d9585f53d24362ba11076f96d42e2539
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2026-02-19 23:01:47 -0600

    feat: add backend integration, fix tests

--- src/courseInfo/CourseInfoPage.tsx
+++ src/courseInfo/CourseInfoPage.tsx
@@ -2,22 +2,12 @@ import { PendingTasks } from '@src/components/PendingTasks';
 import { GeneralCourseInfo } from '@src/courseInfo/components/generalCourseInfo';
 import { EnrollmentSummary } from './components/EnrollmentSummary';
 
-const CourseInfoPage = () => {
-  // TODO: replace this mock data with real data from API
-  const mockEnrollmentCounts = {
-    total: 3640,
-    staffAndAdmins: 10,
-    learners: 3630,
-    verified: 2400,
-    audit: 1230,
-  };
-  return (
-    <div className="mt-4.5 mb-4">
-      <GeneralCourseInfo />
-      <EnrollmentSummary enrollmentCounts={mockEnrollmentCounts} />
-      <PendingTasks />
-    </div>
-  );
-};
+const CourseInfoPage = () => (
+  <>
+    <GeneralCourseInfo />
+    <EnrollmentSummary />
+    <PendingTasks />
+  </>
+);
 
 export default CourseInfoPage;
