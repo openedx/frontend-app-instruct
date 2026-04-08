77a3eae23591e3576e65585eb1472217e457bcf6
diana-villalvazo-wgu <diana.villalvazo@wgu.edu>
2026-04-08 16:18:04 -0600

    feat: add course nav provides

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
 import { App } from '@openedx/frontend-base';
 import { appId } from '@src/constants';
 import routes from '@src/routes';
-import messages from '@src/i18n';
 import slots from '@src/slots';
 import providers from '@src/providers';
 
 const app: App = {
   appId,
   routes,
-  messages,
   providers,
   slots,
   config: {
     NODE_ENV: 'development',
     LMS_BASE_URL: 'http://local.openedx.io:8000'
-  }
+  },
+  provides: {
+    'org.openedx.frontend.provides.courseNavigationRoles.v1': {
+      courseNavigationRoles: ['org.openedx.frontend.role.instructor'],
+    },
+  },
 };
 
 export default app;
